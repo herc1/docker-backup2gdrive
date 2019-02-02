9da9dd25844c28478742c61323b85ccbcdb4ea31
herc1 <hnstoyanov@gmail.com>
2019-02-02 12:59:19 +0200

    add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+touch /tmp/stdout
+tail -f /tmp/stdout &
+yes | cp -f /opt/backup/crontab /etc/crontab
+cron -f &
+while true; do
+ if [ $(pgrep cron | wc -l) -eq 0 ]
+ then
+  echo "cron is not running! Starting"
+  cron -f &
+ fi
+ sleep 5
+done
